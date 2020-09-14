48062708eb18bee03714f3d8d1ced8a9e090f95d
Ryan Arora <arora.ryan@gmail.com>
2020-09-14 18:38:50 -0400

    Refactor prefix code

--- src/client.js
+++ src/client.js
@@ -1,4 +1,4 @@
-if (!process.env.NODE_ENV) require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
 const fs = require('fs');
 const path = require('path');
@@ -8,6 +8,10 @@ const Discord = require('discord.js');
 const readdir = promisify(fs.readdir);
 
 const client = new Discord.Client({ disableEveryone: true });
+
+client.settings = {
+  prefix: '<',
+};
 
 (async () => {
   // Event Loader
