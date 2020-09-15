fdeb5d5320eb9df27f0943435ee7aaaa56e78d33
Ryan Arora <arora.ryan@gmail.com>
2020-09-15 03:38:37 -0400

    made code a bit more readable and made error messages consistent

--- src/client.js
+++ src/client.js
@@ -1,3 +1,4 @@
+// Loads environment variables from .env to setup development environment
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
 const fs = require('fs');
@@ -5,6 +6,7 @@ const path = require('path');
 const { promisify } = require('util');
 const Discord = require('discord.js');
 
+// promisified to use async/await to avoid callback hell
 const readdir = promisify(fs.readdir);
 
 const client = new Discord.Client({ disableEveryone: true });
@@ -19,12 +21,14 @@ client.settings = {
   const evtFiles = await readdir(path.join(__dirname, 'events'));
   console.log(`Loading a total of ${evtFiles.length} events.`);
 
+  // Loops through ./events folder's files
   evtFiles.forEach((file) => {
     if (!file.endsWith('.js')) return;
 
     const evtName = file.split('.')[0];
     const evt = require('./events/' + file);
 
+    // Loads evt
     client.on(evtName, (...args) => {
       evt(client, ...args);
     });
@@ -36,10 +40,13 @@ client.settings = {
 
   client.commands = {};
 
+  // Loops through ./commands folder's files
   cmdFiles.forEach((file) => {
     if (!file.endsWith('.js')) return;
 
     const cmdName = file.split('.')[0];
+
+    // Loads each command function in ./commands into memory that can be accessed from the client object.
     client.commands[cmdName] = require(`./commands/${file}`);
   });
 
--- src/events/message.js
+++ src/events/message.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const message = async (client, message) => {
+  // Prevent bot from responding to other bots and outside of a guild
   if (message.author.bot) return;
   if (!message.guild) return;
 
@@ -26,22 +27,28 @@ const message = async (client, message) => {
     }
   }
 
+  // If you mention the bot, it will tell you its prefix
   const prefixMention = new RegExp(`^<@!?${client.user.id}>( |)$`);
   if (message.content.match(prefixMention)) {
     return message.reply(`My prefix on this server is \`${client.settings.prefix}\``);
   }
 
+  // Ignores any messages without prefix after this line
   if (!message.content.startsWith(client.settings.prefix)) return;
 
+  // Derives args and command from message content
   const args = message.content.trim().slice(client.settings.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 
+  // Puts args into the message object to avoid passing it again into the command function
+  // It looks a bit cleaner without the extra argument in the cmd functions
   message.args = args;
 
+  // If the command entered is loaded, execute it, otherwise tell the user that the command wasn't
   if (client.commands[command]) {
     client.commands[command](client, message);
   } else {
-    message.channel.send(`\`Command not found. Please run ${client.settings.prefix}help for a list of all commands\``);
+    message.channel.send(`\`command not found, run ${client.settings.prefix}help for a list of all commands\``);
   }
 };
 
