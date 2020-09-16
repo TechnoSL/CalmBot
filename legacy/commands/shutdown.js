d97ed97e8d1956095c83e82803d3f33a20b33f0a
Ryan Arora <arora.ryan@gmail.com>
2020-09-15 23:59:02 -0400

    fix: unneeded legacy files deleted

--- legacy/commands/shutdown.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  execute(message, args, bot) {
-    if (message.author.id != '438057670042320896') {
-      return message.channel.send('Error! You are not my owner!');
-    } else {
-      bot.destroy();
-    }
-  },
-};
