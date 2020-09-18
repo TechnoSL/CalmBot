d6c3f783a0fd884eae201dea8d807d3f53b07d7c
Ryan Arora <arora.ryan@gmail.com>
2020-09-18 08:13:00 -0400

    feat: add c!arlo breed subcommand

--- src/commands/arlo.js
+++ src/commands/arlo.js
@@ -1,8 +1,12 @@
 const urls = require('../data/arlo.json');
 
 const arlo = (client, message) => {
-  const img = urls[Math.floor(Math.random() * urls.length)];
-  message.channel.send(img);
+  if (message.args[0].toLowerCase() === 'breed'){
+    message.channel.send('https://cdn.discordapp.com/attachments/501501905508237315/756487167152357376/image0.png');
+  } else {
+    const img = urls[Math.floor(Math.random() * urls.length)];
+    message.channel.send(img);
+  }
 };
 
 module.exports = arlo;
